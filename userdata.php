ee2c94b9f0d6d031d2c0f78732e4ae9554baf133
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-12 23:57:30 -0400

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -4,7 +4,7 @@ include('server.php');
 
 $username = $_SESSION['username'];
 
-$result = mysqli_query($conn,"SELECT password FROM user WHERE username = '$username'");
+$result = mysqli_query($conn,"SELECT weight FROM user WHERE username = '$username'");
 $result = mysqli_fetch_array($result);
 echo json_encode($result);
 
