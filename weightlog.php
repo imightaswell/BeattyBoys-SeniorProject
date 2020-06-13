4ed9a61c2bb7852c9e6bdc4d22a5b791e6539489
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 19:01:10 -0400

    Update weightlog.php

--- weightlog.php
+++ weightlog.php
@@ -8,9 +8,17 @@ include('server.php');
 <!DOCTYPE html>
 <html>
 <head>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 	<title>BeattyBoys Fitness - Weight Log</title>
+</head>
+<body>
 
-	<script type="text/javascript">
+
+
+
+
+
+<script type="text/javascript">
 		
 	var ajax =  new XMLHttpRequest();
 	var method = "GET";
@@ -29,14 +37,36 @@ include('server.php');
 		alert(weight);
 		console.log(weight);
 		
+
+		var ctx = document.getElementById("chart").getContext('2d');
+    		var myChart = new Chart(ctx, {
+        	type: 'line',
+		       data: {
+		           labels: [1,2,3,4,5,6,7,8,9],
+		           datasets: 
+		           [{
+		               label: 'Weight',
+		               data: [weight],
+		               backgroundColor: 'transparent',
+		               borderColor:'rgba(255,99,132)',
+		               borderWidth: 3
+		           },
+
+		           ]
+		       },
+		     
+		       options: {
+		           scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
+		           tooltips:{mode: 'index'},
+		           legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
+		       }
+		   });
+
+
+
 	}
 	}
 	</script>
-
-	
-
-</head>
-<body>
 
 <form method="post" action="weightlog.php">
 
