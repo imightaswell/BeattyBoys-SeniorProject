2eea0ffeec8e46a8bb30c9639caa5af2253ad1ae
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 01:54:27 -0400

    made calculator js file

--- calculator.js
+++ calculator.js
@@ -1,124 +1,62 @@
-/** Declares the variables that will be the BMR, cutting, and bulking calories **/
-var base;
-var cutting;
-var bulking;
+var ajax =  new XMLHttpRequest();
+		var method = "GET";
+		var url = "userdata.php";
+		var asynchronous = true;
 
-/** Declares the macro arrays  **/
+		ajax.open(method, url, asynchronous);
 
-var maintainArray;
-var cuttingArray;
-var bulkingArray;
+		ajax.send();
 
-var height;
-var age;
-var weight;
+		ajax.onreadystatechange = function(){
+			if (this.readyState == 4 && this.status == 200){
+				//alert(this.responseText);
+				var data = JSON.parse(this.responseText);
+				console.log(data);
+				var base;
+				var cutting;
+				var bulking;
 
-function calculator(){
+				var age = data[0];
+				var weight = data[1];
+				var height = data[2];
+				var gender = data[3];
+				var activity = data[4];
+				
+				weight = weight/2.205;
+				height = height*2.54;
 
-/** Declares the gender variable that is true or false if the male radio button is selected or not **/
-var gender = new Boolean(document.getElementById("genderM").checked);
-
-/** Grabs the raw weight in pounds from the form **/
-var weightInPounds = document.getElementById("weight").value;
-
-
-/** Grabs the height (gets converted to cm), age, weight (gets converted to kg), and activity level from the form **/
-height = document.getElementById("height").value*2.54;
-age = document.getElementById("age").value;
-weight = document.getElementById("weight").value/2.205;
-var activity = document.getElementById("activity").value;
-
-/** Declares the multiplier variable that sets to a specific number depending on what activity level was selected **/
-var multiplier;
+				var multiplier;
 
 
-if (activity == "Sedentary"){
-	multiplier = 1.2;
-}
+				if (activity == "Sedentary"){
+					multiplier = 1.2;
+				}
 
-else if (activity == "Moderate"){
-	multiplier = 1.4;
-}
+				else if (activity == "Moderate"){
+					multiplier = 1.4;
+				}
 
-else if (activity == "Intense"){
-	multiplier = 1.7;
-}
+				else if (activity == "Intense"){
+					multiplier = 1.7;
+				}
 
-else {
-	multiplier = 1.95;
-}
+				else {
+					multiplier = 1.95;
+				}
 
-/** Calculates the BMR calories using the Mifflin-St Jeor equation depending on the gender of the user **/
-if (gender==true){
-	base=((10*weight+6.25*height-5*age) + 5) * multiplier;
-}
+				if (gender=="Male"){
+					base=((10*weight+6.25*height-5*age) + 5) * multiplier;
+				}
 
-else {
-	base=((10*weight+6.25*height-5*age) - 161) * multiplier;
-}
+				else {
+					base=((10*weight+6.25*height-5*age) - 161) * multiplier;
+				}
 
-/** Determines the cutting and bulking calories by either taking away 20% from the BMR or adding 15% to the BMR **/
-cutting = base - (base*.2);
-bulking = base + (base*.15);
+				cutting = base - (base*.2);
+				bulking = base + (base*.15);
 
-/** A nested function that takes a set amount of calories "c" and a set weight "w" and calculates 
-	the macro nutrients a person should consume daily **/
-function macros(c, w){
-
-/** Creates an array that will supply the calculated. The array will look like: [protein, fats, carbs]**/
-var array = new Array();
-
-/** Calculates the protein (1g per pound of body weight) then takes away the calories from the protein from the total **/
-var protein = w;
-array.push(protein);
-var leftOver = c - (protein*4);
-
-/** Calculates the fats (25% of caloric intake) then takes away the calories from the fats from the remaining calories **/
-var fats = ((c*.25)/9).toFixed(0);
-array.push(fats);
-leftOver = leftOver - (fats*9);
-
-/** Calculates the carbs (remaining calories) then takes away the calories from the fats from the remaining calories **/
-var carbs = (leftOver/4).toFixed(0);
-array.push(carbs);
-
-return array;
-}
-
-/** calls the function for the base, cutting, and bulking calories **/
-baseArray = macros(base, weightInPounds);
-cuttingArray = macros(cutting, weightInPounds);
-bulkingArray = macros(bulking, weightInPounds);
-
-}
-
-
-/** Creates an event lister that listens for the calculate button to be clicked then prints the results after the click  **/
-document.getElementsByTagName("button")[0].addEventListener("click", function() {
-	
-	calculator();
-
-	/** Ensures that all inputs are filled and numerical**/
-	if (weight=="" || age=="" || height==""){
-		alert("Please Fill in All Fields");
-	}
-
-	else {
-	document.getElementById('baseCals').innerHTML = base.toFixed(0);
-	document.getElementById('cuttingCals').innerHTML = cutting.toFixed(0);
-	document.getElementById('bulkingCals').innerHTML = bulking.toFixed(0);
-
-	document.getElementById('baseProtein').innerHTML = baseArray[0];
-	document.getElementById('baseFats').innerHTML = baseArray[1];
-	document.getElementById('baseCarbs').innerHTML = baseArray[2];
-
-	document.getElementById('cuttingProtein').innerHTML = cuttingArray[0];
-	document.getElementById('cuttingFats').innerHTML = cuttingArray[1];
-	document.getElementById('cuttingCarbs').innerHTML = cuttingArray[2];
-
-	document.getElementById('bulkingProtein').innerHTML = bulkingArray[0];
-	document.getElementById('bulkingFats').innerHTML = bulkingArray[1];
-	document.getElementById('bulkingCarbs').innerHTML = bulkingArray[2];
-	}
-
-})
+				document.getElementById('baseCals').innerHTML = base.toFixed(0);
+				document.getElementById('cuttingCals').innerHTML = cutting.toFixed(0);
+				document.getElementById('bulkingCals').innerHTML = bulking.toFixed(0);
+			}
+		}
--- calories.php
+++ calories.php
@@ -5,70 +5,7 @@
 <head>
 	<title>BeattyBoys Fitness -Calories & Macronutrients</title>
 
-	<script>
-		var ajax =  new XMLHttpRequest();
-		var method = "GET";
-		var url = "userdata.php";
-		var asynchronous = true;
-
-		ajax.open(method, url, asynchronous);
-
-		ajax.send();
-
-		ajax.onreadystatechange = function(){
-			if (this.readyState == 4 && this.status == 200){
-				//alert(this.responseText);
-				var data = JSON.parse(this.responseText);
-				console.log(data);
-				var base;
-				var cutting;
-				var bulking;
-
-				var age = data[0];
-				var weight = data[1];
-				var height = data[2];
-				var gender = data[3];
-				var activity = data[4];
-				
-				weight = weight/2.205;
-				height = height*2.54;
-
-				var multiplier;
-
-
-				if (activity == "Sedentary"){
-					multiplier = 1.2;
-				}
-
-				else if (activity == "Moderate"){
-					multiplier = 1.4;
-				}
-
-				else if (activity == "Intense"){
-					multiplier = 1.7;
-				}
-
-				else {
-					multiplier = 1.95;
-				}
-
-				if (gender=="Male"){
-					base=((10*weight+6.25*height-5*age) + 5) * multiplier;
-				}
-
-				else {
-					base=((10*weight+6.25*height-5*age) - 161) * multiplier;
-				}
-
-				cutting = base - (base*.2);
-				bulking = base + (base*.15);
-
-				document.getElementById('baseCals').innerHTML = base.toFixed(0);
-				document.getElementById('cuttingCals').innerHTML = cutting.toFixed(0);
-				document.getElementById('bulkingCals').innerHTML = bulking.toFixed(0);
-			}
-		}
-	</script>
+	<script type="text/javascript" src="calculator.js"></script>
 
 </head>
 <body>
