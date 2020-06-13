9d7acd6e2d37b40b4663f9447bb7730d919636d6
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 00:12:10 -0400

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -4,8 +4,13 @@ include('server.php');
 
 $username = $_SESSION['username'];
 
-$result = mysqli_query($conn,"SELECT gender FROM user WHERE username = '$username'");
+$result = mysqli_query($conn,"SELECT * FROM user WHERE username = '$username'");
 $result = mysqli_fetch_assoc($result);
 echo json_encode($result['gender']);
 
+$age = "";
+$weight = "";
+$height = "";
+$gender = "";
+$activity = "";
  ?>
