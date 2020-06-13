ce4072df765765df460694f5b3edc1aa2356a4be
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 00:06:11 -0400

    Update calories.php

--- calories.php
+++ calories.php
@@ -17,9 +17,9 @@
 
 		ajax.onreadystatechange = function(){
 			if (this.readyState == 4 && this.status == 200){
-				//alert(this.responseText);
-				var data = JSON.parse(this.responseText);
-				console.log(data);
+				alert(this.responseText);
+				//var data = JSON.parse(this.responseText);
+				//console.log(data);
 			}
 		}
 
