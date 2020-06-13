2cc5d3f0038c04234837dd46062c3d06369de2e7
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 00:09:11 -0400

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -6,6 +6,8 @@ $username = $_SESSION['username'];
 
 $result = mysqli_query($conn,"SELECT gender FROM user WHERE username = '$username'");
 $result = mysqli_fetch_assoc($result);
-echo json_encode($result);
+
+while ($row = $result->fetch_array(MYSQLI_ASSOC))
+echo json_encode($row['gender']);
 
  ?>
