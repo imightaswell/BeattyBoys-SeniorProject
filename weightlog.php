9dafad68a517c46a41ffb9251d18f1af8d446d09
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 19:35:26 -0400

    Update weightlog.php

--- weightlog.php
+++ weightlog.php
@@ -46,18 +46,7 @@ include('server.php');
 			type: "line",
       		indexLabelFontSize: 16,
 			dataPoints: [
-				{ y: 450 },
-				{ y: 414},
-				{ y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
-				{ y: 460 },
-				{ y: 450 },
-				{ y: 500 },
-				{ y: 480 },
-				{ y: 480 },
-				{ y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-				{ y: 500 },
-				{ y: 480 },
-				{ y: 510 }
+				weight
 				]
 			}]
 		});
