c82f4a54df26edca55761766f6fc7fc9b8fc4571
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-12 21:52:01 -0400

    testing ajax

--- calories.php
+++ calories.php
@@ -8,7 +8,7 @@
 	<script>
 		var ajax =  new XMLHttpRequest();
 		var method = "GET";
-		var url = "userdata.php";
+		var url = "server.php";
 		var asynchronous = true;
 
 		ajax.open(method, url, asynchronous);
