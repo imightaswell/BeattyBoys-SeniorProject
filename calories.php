bf56a50f3c547956d410d978af000c443f2e6bd8
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 01:01:16 -0400

    Update calories.php

--- calories.php
+++ calories.php
@@ -19,6 +19,10 @@
 			if (this.readyState == 4 && this.status == 200){
 				//alert(this.responseText);
 				var data = JSON.parse(this.responseText);
+				console.log(data);
+				var base;
+				var cutting;
+				var bulking;
 
 				var age = data[0];
 				var weight = data[1];
@@ -27,8 +31,39 @@
 				var activity = data[4];
 				
 				weight = weight/2.205;
-				alert(weight);
-				console.log(data);
+				height = height*2.54;
+
+				var multiplier;
+
+
+				if (activity == "Sedentary"){
+					multiplier = 1.2;
+				}
+
+				else if (activity == "Moderate"){
+					multiplier = 1.4;
+				}
+
+				else if (activity == "Intense"){
+					multiplier = 1.7;
+				}
+
+				else {
+					multiplier = 1.95;
+				}
+
+				if (gender==true){
+					base=((10*weight+6.25*height-5*age) + 5) * multiplier;
+				}
+
+				else {
+					base=((10*weight+6.25*height-5*age) - 161) * multiplier;
+				}
+
+				cutting = base - (base*.2);
+				bulking = base + (base*.15);
+
+				alert(base);
 			}
 		}
 	</script>
