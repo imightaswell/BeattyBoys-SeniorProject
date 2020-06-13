b01e07a39220cb4f11962a47050d078af78804f4
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 18:08:53 -0400

    Update server.php

--- server.php
+++ server.php
@@ -49,7 +49,7 @@ if(isset($_POST['signup'])){
 		mysqli_query($conn,$query);
 
 		$_SESSION['username'] = $username;
-		$_SESSION['password'] = $password;
+		
 		$_SESSION['success'] = "You have logged in";
 
 		header('location: index.php');
@@ -77,7 +77,7 @@ if (isset($_POST['signin'])) {
 		if(mysqli_num_rows($result)==1){
 			$_SESSION['username'] = $username;
 			$_SESSION['success'] = "You have logged in";
-			$_SESSION['password'] = $password;
+		
 			header('location: index.php');
 
 		}
@@ -98,7 +98,11 @@ if (isset($_POST['weightChange'])){
 	$weight = $result['weight'];
 
 	$weight = $weight . "," . $_POST['weight'];
-	echo json_encode($weight);
+
+	$query = "UPDATE user SET weight='$weight' WHERE username='$username'";
+	mysqli_query($conn,$query);
+
+
 }
 
  ?>
