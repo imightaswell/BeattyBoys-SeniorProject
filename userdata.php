800c1622a9450067387a04ca3e779fd0fd6c6541
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 18:18:42 -0400

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -9,7 +9,19 @@ $result = mysqli_fetch_assoc($result);
 
 
 $age = $result['age'];
-$weight = $result['weight'];
+
+$weight_array = array();
+
+$weight_string = $result['weight'];
+
+$weight_array = explode(",", $weight_string);
+
+//weight_array[0] = 172
+//weight_array[1] = 170
+
+$relevant_weight = sizeof($weight_array)-1;
+
+$weight = $weight_array[$relevant_weight];
 $height = $result['height'];
 $gender = $result['gender'];
 $activity = $result['activity_level'];
