0bf5e53f470316029d04b80faad34bd91551bbb8
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-14 19:47:04 -0400

    Update weight.js

--- weight.js
+++ weight.js
@@ -28,7 +28,7 @@ var ajax =  new XMLHttpRequest();
 
 		var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
