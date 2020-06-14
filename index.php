1113b174e3206b021c243fc541c9584d367e9a1a
imightaswell <62396252+imightaswell@users.noreply.github.com>
2020-06-14 02:15:25 -0400

    button test again

--- index.php
+++ index.php
@@ -63,7 +63,22 @@ if (isset($_GET['logout'])) {
 		<p class= Username><strong><?php echo $_SESSION['username']; ?></strong></p> 
 		<a href="index.php?logout='1'" class="logout">Logout</a>
 
+		<figure class="snip1321"><img src="img/food.jpeg" alt="food"/>
+		  <figcaption><i class="ion-pizza"></i>
+		    <h4>Caloric Intake</h4>
+		    <h2>stuff</h2>
+		  </figcaption><a href="calories.php"></a>
+		</figure>
+
 		<div class="buttonLOGIN">
+
+		<figure class="snip1321"><img src="img/food.jpeg" alt="food"/>
+		  <figcaption><i class="ion-pizza"></i>
+		    <h4>Caloric Intake</h4>
+		    <h2>stuff</h2>
+		  </figcaption><a href="calories.php"></a>
+		</figure>
+
 			<a href="calories.php" class="btnLOGIN">Caloric Intake & Macronutrients</a> 
 			<a href="weightlog.php" class="btnLOGIN">Weight Log</a> 
 			<a href="routines.php" class="btnLOGIN">Workout Routines</a>  
