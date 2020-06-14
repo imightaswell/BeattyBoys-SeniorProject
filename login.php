0a8da9d9da0a7dd943f9b379f277d8991a7c3761
BrianRobles388 <62396760+BrianRobles388@users.noreply.github.com>
2020-06-13 21:26:41 -0400

    rough Draft login  css

--- login.php
+++ login.php
@@ -5,35 +5,50 @@
 <head lang="en">
 	<meta charset="utf-8">
 	<title>Beatty Boyz Fitness - Login</title>
-	<link rel="stylesheet" href=""/>
+	<link rel="stylesheet" href="css/css_login.css"/>
 </head>
 
+
 <body>
-	<header>
+
+	<!-- Video Background Source -->
+	<header class="fix">
+		<video src="img/loginbg.mp4" class="vid-bg" autoplay loop muted></video>
+		<div class="grayfilter">
+
+		<!-- Navi Bar -->
+		<div class ="blackbar">
+		</div>
 		<div class="main">
 			<div class="logo">
-				<img src="img/logo1.png">
+				<a href="index.php">
+				<img src="img/logo2.png"></img>
+				</a>
 			</div>
 			<ul>
-				<li class="active"><a href="index.php">Home</a></li>
-				<li><a href="about.php">About Us</a></li>
+				<li><a href="#">About Us</a></li>
 			</ul>
 		</div>
-	</header>
-
-	<form action="login.php" method="post">
-		<?php include('errors.php'); ?> 
-		<label for="username">Username:</label><br>
-		<input type="text" name="username"><br>
-		<label for="password">Password:</label><br>
-		<input type="password" name="password"><br>
-		<button type="submit" name="signin">Login</button> 
-	</form>
-
-	<div>
-		<p>New user? <a href="register.php">Register here</a>.</p>
 	</div>
+
+	<!-- Login Form -->
+		<div class="loginform">
+			<form action="login.php" method="post">
+
+				<img src="img/logo3.png" class="avatar">
+				<div class="errors">
+				<?php include('errors.php'); ?>
+			    </div>
+
+				<label for="username">Username</label><br>
+				<input type="text" name="username"><br>
+				<label for="password">Password</label><br>
+				<input type="password" name="password"><br>
+				<button type="submit" name="signin" value="login">Login</button> 
+				<p>New user? <a href="register.php">Register here</a>.</p>
+			</form>
+    	</div>
+
+</header>
 </body>
-
 </html>
-
