31361c49a49fa0c98c206b5accdc9f44faccabdc
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 22:29:36 -0400

    Update weight.js

--- weight.js
+++ weight.js
@@ -16,6 +16,18 @@ var ajax =  new XMLHttpRequest();
 		for(var i=0;i<weight.length;i++){
 			weight[i] = parseFloat(weight[i]);
 		}
+
+		var chart = new CanvasJS.Chart("chartContainer", {
+ 		 title: {
+    		text: " Populating chart using array "
+  		},
+  		data: [{
+    		type: "line",
+    		dataPoints: weight
+  		}]
+		});
+
+		chart.render();
 		console.log(weight);
 	}
 
