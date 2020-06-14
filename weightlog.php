de5d475cdb05e34f758e2b3109250d7f7048ba62
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-13 20:36:18 -0400

    Update weightlog.php

--- weightlog.php
+++ weightlog.php
@@ -4,20 +4,8 @@
 <head>
 	<title>BeattyBoys Fitness - Weight Log</title>
 
-	<script type="text/javascript">
-		var ajax =  new XMLHttpRequest();
-		var method = "GET";
-		var url = "server.php";
-		var asynchronous = true;
-
-		ajax.open(method, url, asynchronous);
-
-		ajax.send();
-
-		ajax.onreadystatechange = function(){
-			var weight =  new Array();
-			weight = JSON.parse(this.responseText);	
-		}
+	<script src="weight.js">
+		
 	</script>
 </head>
 <body>
