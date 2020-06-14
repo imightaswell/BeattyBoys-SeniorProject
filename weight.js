db8d64db2f4330d2ca7c306a99ab11641f363dff
imightaswell <62396252+imightaswell@users.noreply.github.com>
2020-06-14 13:04:28 -0400

    Update weight.js

--- weight.js
+++ weight.js
@@ -9,38 +9,35 @@ var ajax =  new XMLHttpRequest();
 
 
 	ajax.onreadystatechange = function(){
-		if(ajax.readyState == 4 && xhttp.status == 200) {
-        var data = JSON.parse(this.responseText);
+		var data = JSON.parse(this.responseText);
 		
 		var weight =  data[1].split(",");
 
 		for(var i=0;i<weight.length;i++){
 			weight[i] = parseFloat(weight[i]);
-			$dataPoints = weight[i];
 		}
-    }
 
-window.onload = function () {
+		window.onload = function () {
 
-	var chart = new CanvasJS.Chart("chartContainer", {
-		animationEnabled: true,
-		theme: "light2",
-		title:{
-			text: "Simple Line Chart"
-		},
-		axisY:{
-			includeZero: false
-		},
-		data: [{        
-			type: "line",
-	      	indexLabelFontSize: 16,
-			dataPoints: [weight]
-		}]
-	});
-	chart.render();
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Simple Line Chart"
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{        
+		type: "line",
+      	indexLabelFontSize: 16,
+		dataPoints: [weight]
+	}]
+});
+chart.render();
 
 }
 
-	
+	}
 
-}	
+	
