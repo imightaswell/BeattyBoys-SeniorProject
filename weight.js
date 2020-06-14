bb055ec9b6dfe954b29dcb181c5149e10dacbdc2
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-14 17:29:44 -0400

    Update weight.js

--- weight.js
+++ weight.js
@@ -15,44 +15,30 @@ var ajax =  new XMLHttpRequest();
 
 		weight = weight.substring(1, weight.length-1);
 		var weight_array = weight.split(".");
-
 		
-		
-		
+		window.onload = function () {
 
-
-		//for(var i=0;i<weight.length;i++){
-			//weight[i] = parseFloat(weight[i]);
-		//}
-		console.log(weight_array);
-		console.log(weight_array[0]*2);
-		
-		//alert(Array.isArray(userdata));
-		
-
-		//window.onload = function () {
-
-		//var chart = new CanvasJS.Chart("chartContainer", {
-			//animationEnabled: true,
-			//theme: "light2",
-			//title:{
-				//text: "Simple Line Chart"
-			//},
-			//axisY:{
-				//includeZero: false
-			//},
-			//data: [{        
-				//type: "line",
-      			//indexLabelFontSize: 16,
-				//datasets: [
-				//{
-				//	label: "Weight Entries"
-				//	data: [1,2,3]
-				//}
-		//	}]
-		//	});
-		//chart.render();
-		//}
+		var chart = new CanvasJS.Chart("chartContainer", {
+			animationEnabled: true,
+			theme: "light2",
+			title:{
+				text: "Simple Line Chart"
+			},
+			axisY:{
+				includeZero: false
+			},
+			data: [{        
+				type: "line",
+      			indexLabelFontSize: 16,
+				datasets: [
+				{
+					label: "Weight Entries"
+					data: [1,2,3]
+				}
+			}]
+			});
+		chart.render();
+		}
 	}
 
 	
