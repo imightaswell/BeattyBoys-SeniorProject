9dc45d4bb941ea8514603f88b798f0c516fc123e
BrianRobles388 <62396760+BrianRobles388@users.noreply.github.com>
2020-06-15 00:46:02 -0400

    Register page CSS fini
    Might need some tweaking but good enough for now

--- register.php
+++ register.php
@@ -3,54 +3,97 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title></title>
+    <meta charset="utf-8">
+	<title>Beatty Boyz Fitness - Register</title>
+    <link rel="stylesheet" href="css/css_register.css"/>
 </head>
+    
 <body>
+    
+    	<!-- Video Background Source -->
+    <video src="img/loginbg.mp4" class="vid-bg" autoplay loop muted></video>
+    
+    <!-- Filter Background -->
+    <div class="grayfilter">
+
+    <!-- Navi Bar -->
+		<div class ="blackbar">
+        </div>
+    
+    <!-- Main -->
+    <div class="main">
+        
+    <!-- Logo -->
+			<div class="logo">
+				<a href="index.php"> <img src="img/logo2.png"></img> </a>
+			</div>
+        
+    <!-- About Us text -->
+			<ul>
+				<li><a href="#">About Us</a></li>
+			</ul>
+    </div>
+
+	<!-- Login Form -->
+		<div class="loginform">
+            
+    <!-- Form -->    
 	<form method="post" action="register.php">
+        
+        <img src="img/Logo4.png" class="avatar">
+        <div class="errors">
+		<?php include('errors.php'); ?>
+        </div>
 
-		<?php include('errors.php'); ?> 
+		<label for="username"></label><br/>
+		<input name="username" type="text" placeholder="Username" /><br />
 
-		<label for="username">Username:</label><br/>
-		<input name="username" type="text" /><br />
+		<label for="password"></label><br />
+		<input name="password" type="password" placeholder="Password"/><br />
 
-		<label for="password">Password:</label><br />
-		<input name="password" type="password" /><br />
-
-		<label for="age">Age:</label><br />
-		<input name="age" type="number" /><br />
-
-		<label for="height">Your Height in Inches:</label><br />
-		<input name="height" type="number" step="any" /><br />
-
-		<label for="weight">Your Weight in Pounds:</label><br />
-		<input name="weight" type="number" /><br />
-
-		<label for="gender">Gender:</label> 
-			<select name="gender">
+		<label for="age"></label><br />
+		<input name="age" type="number" placeholder="Age" /><br />
+        
+		<label for="height"></label><br />
+		<input name="height" type="number" step="any" placeholder="Height (Inches)"/><br />
+        
+		<label for="weight"></label><br />
+		<input name="weight" type="number" placeholder="Weight (Pounds)"/><br />
+        
+        <!-- Select CSS HTML -->
+        
+            <div class="dds">
+		<label for="gender"></label>
+			<select name="gender" id="gender" data-selected="">
+                <option style="color:gray" selected="selected" disabled="disabled">Select Gender</option>
 				<option>Male</option>
 				<option>Female</option> 
-			</select><br>
-
-		<label for="activtiylevel">Activity Level:</label> 
+			</select>
+        
+		<label for="activtiylevel"></label> 
 			<select name="activity">
+                <option selected="selected" disabled="disabled"> Select Activity Level</option>
 				<option>Sedentary</option>
 				<option>Moderate</option>
 				<option>Intense</option>
 				<option>Athlete</option> 
 			</select><br> 
 			
-		<label for="fitness_goal">Fitness Goal:</label> 
+		<label for="fitness_goal"></label> 
 			<select name="fitness_goal">
+                <option selected="selected" disabled="disabled">Select Fitness Goal</option>
 				<option>Hypertrophy</option>
 				<option>Cardiovascular Health</option>
 				<option>Strength</option>
 			</select><br>
-
+        </div>
 		<button type="submit" name="signup">Register</button> 
 	</form>
-
+        
+ </div>
 	<div>
 		<p>Already have an account? <a href="login.php">Login here</a></p>
 	</div>
+    
 </body>
 </html>
