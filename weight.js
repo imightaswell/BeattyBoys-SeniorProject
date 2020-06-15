90de15888e61a088137c752830d1bf7e1e8dfcff
ValenzuelaE <62396808+ValenzuelaE@users.noreply.github.com>
2020-06-14 22:43:25 -0400

    Update weight.js

--- weight.js
+++ weight.js
@@ -44,13 +44,9 @@ var myChart = new Chart(ctx, {
                 'rgb(0,0,0)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(0,0,0)'
             ],
+            borderWidth: 1,
             lineTension: 0,
             fill : false
         }]
@@ -59,7 +55,10 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: false
+                    beginAtZero: true,
+                    callback: function(value, index, values) {
+                        return 'Entry ' + value;
+                    }
                 }
             }]
         }
